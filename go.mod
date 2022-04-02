88caab42d1300ec48ae30f7079132dffba5a6442
Rafał Korkosz <korkosz.rafal@gmail.com>
2022-04-02 15:58:03 +0200

    implement multiple hosts proxy
    Signed-off-by: Rafał Korkosz <korkosz.rafal@gmail.com>

--- go.mod
+++ go.mod
@@ -2,26 +2,29 @@ module github.com/rkorkosz/rpoxy
 
 go 1.18
 
-require github.com/rkorkosz/web v0.0.0-20220401214622-d2e7cd9fe909
+require (
+	github.com/rkorkosz/web v0.0.0-20220402124831-d7d69197863e
+	gopkg.in/yaml.v3 v3.0.0-20210107192922-496545a6307b
+)
 
 require (
 	github.com/cheekybits/genny v1.0.0 // indirect
 	github.com/francoispqt/gojay v1.2.13 // indirect
-	github.com/fsnotify/fsnotify v1.4.9 // indirect
+	github.com/fsnotify/fsnotify v1.5.1 // indirect
 	github.com/go-task/slim-sprig v0.0.0-20210107165309-348f09dbbbc0 // indirect
-	github.com/lucas-clemente/quic-go v0.25.0 // indirect
+	github.com/lucas-clemente/quic-go v0.26.0 // indirect
 	github.com/marten-seemann/qpack v0.2.1 // indirect
-	github.com/marten-seemann/qtls-go1-16 v0.1.4 // indirect
-	github.com/marten-seemann/qtls-go1-17 v0.1.0 // indirect
-	github.com/marten-seemann/qtls-go1-18 v0.1.0-beta.1 // indirect
+	github.com/marten-seemann/qtls-go1-16 v0.1.5 // indirect
+	github.com/marten-seemann/qtls-go1-17 v0.1.1 // indirect
+	github.com/marten-seemann/qtls-go1-18 v0.1.1 // indirect
 	github.com/nxadm/tail v1.4.8 // indirect
-	github.com/onsi/ginkgo v1.16.4 // indirect
-	golang.org/x/crypto v0.0.0-20220214200702-86341886e292 // indirect
-	golang.org/x/mod v0.4.2 // indirect
-	golang.org/x/net v0.0.0-20211112202133-69e39bad7dc2 // indirect
-	golang.org/x/sys v0.0.0-20210615035016-665e8c7367d1 // indirect
-	golang.org/x/text v0.3.6 // indirect
-	golang.org/x/tools v0.1.1 // indirect
+	github.com/onsi/ginkgo v1.16.5 // indirect
+	golang.org/x/crypto v0.0.0-20220331220935-ae2d96664a29 // indirect
+	golang.org/x/mod v0.6.0-dev.0.20220106191415-9b9b3d81d5e3 // indirect
+	golang.org/x/net v0.0.0-20220401154927-543a649e0bdd // indirect
+	golang.org/x/sys v0.0.0-20220330033206-e17cdc41300f // indirect
+	golang.org/x/text v0.3.7 // indirect
+	golang.org/x/tools v0.1.10 // indirect
 	golang.org/x/xerrors v0.0.0-20200804184101-5ec99f83aff1 // indirect
 	gopkg.in/tomb.v1 v1.0.0-20141024135613-dd632973f1e7 // indirect
 )
--- main.go
+++ main.go
@@ -2,37 +2,92 @@ package main
 
 import (
 	"crypto/tls"
+	"errors"
 	"flag"
 	"log"
 	"net/http"
 	"net/http/httputil"
 	"net/url"
+	"os"
 
 	"github.com/rkorkosz/web"
+	"gopkg.in/yaml.v3"
 )
 
 func main() {
-	addr := flag.String("bind", ":8001", "bind address")
-	cert := flag.String("cert", "cert.pem", "certificate")
-	key := flag.String("key", "key.pem", "certificate key")
-	email := flag.String("email", "", "acme email")
-	host := flag.String("host", "", "host for certificate")
+	conf := flag.String("c", "rpoxy.yaml", "config file")
 	flag.Parse()
-	target, err := url.Parse(flag.Arg(0))
+	config, err := InitConfig(*conf)
 	if err != nil {
 		log.Fatal(err)
 	}
-	var tlsConfig *tls.Config
-	if *cert != "" {
-		tlsConfig = web.LocalTLSConfig(*cert, *key)
-	}
-	if *email != "" {
-		tlsConfig = web.AutoCertTLSConfig(*email, *host)
-	}
 	srv := &http.Server{
-		TLSConfig: tlsConfig,
-		Handler:   httputil.NewSingleHostReverseProxy(target),
-		Addr:      *addr,
+		TLSConfig: config.tlsConfig,
+		Handler:   MultiHostProxy(config.Hosts),
+		Addr:      config.Addr,
 	}
 	log.Fatal(srv.ListenAndServeTLS("", ""))
+}
+
+func MultiHostProxy(targets map[string]URL) *httputil.ReverseProxy {
+	director := func(r *http.Request) {
+		target := targets[r.Host]
+		r.URL.Scheme = target.Scheme
+		r.URL.Host = target.Host
+		if _, ok := r.Header["User-Agent"]; !ok {
+			r.Header.Set("User-Agent", "")
+		}
+	}
+	return &httputil.ReverseProxy{Director: director}
+}
+
+type URL url.URL
+
+func (u *URL) UnmarshalText(b []byte) error {
+	parsed, err := url.Parse(string(b))
+	if err != nil {
+		return err
+	}
+	*u = URL(*parsed)
+	return nil
+}
+
+type Config struct {
+	Addr      string
+	Email     string
+	Hosts     map[string]URL
+	Cert, Key string
+
+	tlsConfig *tls.Config
+}
+
+func InitConfig(path string) (*Config, error) {
+	f, err := os.Open(path)
+	if err != nil {
+		return nil, err
+	}
+	defer f.Close()
+	var conf Config
+	err = yaml.NewDecoder(f).Decode(&conf)
+	if err != nil {
+		return nil, err
+	}
+	if conf.Email != "" {
+		var hosts []string
+		for host := range conf.Hosts {
+			hosts = append(hosts, host)
+		}
+		conf.tlsConfig = web.AutoCertTLSConfig(conf.Email, hosts...)
+	} else {
+		conf.tlsConfig = web.LocalTLSConfig(conf.Cert, conf.Key)
+	}
+	err = validateConfig(&conf)
+	return &conf, err
+}
+
+func validateConfig(conf *Config) error {
+	if conf.Email == "" && conf.Cert == "" {
+		return errors.New("you need to provide either acme or local config")
+	}
+	return nil
 }
